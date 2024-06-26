96b23fc51ad1fa70f9e51b49f3cae47ca836c37f
Antoine Thompson <antoine.thompson@outlook.fr>
2024-06-27 01:01:51 +0200

    pump + Dockerfile pour DL Chrome

--- src/main/java/fr/athompson/ffbbscraper/scrapers/Scraper.java
+++ src/main/java/fr/athompson/ffbbscraper/scrapers/Scraper.java
@@ -1,28 +1,22 @@
 package fr.athompson.ffbbscraper.scrapers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.ProtocolHandshake;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.net.URI;
+
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public abstract class Scraper {
 
     protected Document getDocument(String uri) throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //capabilities.setJavascriptEnabled(true);
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:\\Users\\Antoine\\Downloads\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin");
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--webdriver-loglevel=NONE"});
-        Logger.getLogger(PhantomJSDriverService.class.getName()).setLevel(Level.OFF);
-        Logger.getLogger(ProtocolHandshake.class.getName()).setLevel(Level.OFF);
-        WebDriver driver = new PhantomJSDriver(capabilities);
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        var driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         try {
             driver.get(uri);
